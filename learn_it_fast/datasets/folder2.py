5816f7468b133d0b536cd3d7e4110575167e5c47
limyunkai19 <yunkai96@hotmail.com>
2018-04-25 15:45:38 +0800

    Fix data augmentation bug in test set folder2

--- learn_it_fast/datasets/folder2.py
+++ learn_it_fast/datasets/folder2.py
@@ -2,12 +2,12 @@ import os
 from .folder import folder
 
 
-def folder2(root, train_dir='train', test_dir='val', sample_per_class=(-1, -1), **kwargs):
+def folder2(root, train_dir='train', test_dir='val', data_augmentation=False, sample_per_class=(-1, -1), **kwargs):
     train_data = os.path.join(root, train_dir)
-    train_loader = folder(train_data, sample_per_class=sample_per_class[0], **kwargs)
+    train_loader = folder(train_data, data_augmentation=data_augmentation, sample_per_class=sample_per_class[0], **kwargs)
 
     test_data = os.path.join(root, test_dir)
-    test_loader = folder(test_data, sample_per_class=sample_per_class[1], **kwargs)
+    test_loader = folder(test_data, data_augmentation=False, sample_per_class=sample_per_class[1], **kwargs)
 
 
     return train_loader, test_loader
